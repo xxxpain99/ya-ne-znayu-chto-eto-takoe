f370315a569719cfaf59d9ddaf84415f5c89079f
xxxpain99 <84181647+xxxpain99@users.noreply.github.com>
2021-05-24 11:46:46 +0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿public class Class1
+{
+	public Class1()
+	{
+		public void heroes()
+		{
+			hero techies = new hero("Goblini", "colyasochnik", "bad buy", "Salo rink", "Aganimus");
+		    hero pudge = new hero("fat", "angry", "Mr. Black", "hook", "MOM", "BF");
+			creep Meleshnik = new creep("malenkiy", "slabiy");
+		}
+	}
+}
